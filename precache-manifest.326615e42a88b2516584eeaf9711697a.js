d70b758cddb96765730fe11190045608f088d2a9
Jericho FIllon <jerichofilloncoding@gmail.com>
2022-11-02 19:10:49 -0700

    Updates

---
+++ precache-manifest.326615e42a88b2516584eeaf9711697a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "778800cf3a6ceb598b1b",
+    "url": "/jerichofillon/static/css/main.4fddea5f.chunk.css"
+  },
+  {
+    "revision": "778800cf3a6ceb598b1b",
+    "url": "/jerichofillon/static/js/main.778800cf.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/jerichofillon/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "7b78f170963d2f89bb52",
+    "url": "/jerichofillon/static/js/2.7b78f170.chunk.js"
+  },
+  {
+    "revision": "02cd572e2740db2ef4c9",
+    "url": "/jerichofillon/static/js/runtime~main.02cd572e.js"
+  },
+  {
+    "revision": "fc01badb6c0a925d170688372ea8afd0",
+    "url": "/jerichofillon/static/media/avatar.fc01badb.png"
+  },
+  {
+    "revision": "d13ed368d0f50e214a7537ecca0f3841",
+    "url": "/jerichofillon/static/media/noise.d13ed368.png"
+  },
+  {
+    "revision": "bcd70954b82849f307fe308722cd26ec",
+    "url": "/jerichofillon/static/media/tunnel.bcd70954.svg"
+  },
+  {
+    "revision": "f1819251fc1e1446260f0e41cad74471",
+    "url": "/jerichofillon/static/media/speech-bubble.f1819251.svg"
+  },
+  {
+    "revision": "90b59fcdaa47c3812268231f9b9c6576",
+    "url": "/jerichofillon/index.html"
+  }
+];
