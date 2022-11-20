c50357217f91eb8eacccdd95ca02ab7bb5e7ca53
Jericho FIllon <jerichofilloncoding@gmail.com>
2022-11-19 23:29:40 -0800

    Final folder structure

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,8 @@ import portalJpg from "../images/portals.jpg";
 import portalWebp from "../images/portals.webp";
 import portalMobile from "../images/portal-mobile.jpeg";
 
-import Portal from "../components/Hero/Portal/portal.js";
-import Portfolio from "../components/Hero/Portfolio/portfolio.js";
+import Portal from "../components/hero/portal/portal.js";
+import Portfolio from "../components/hero/portfolio/portfolio.js";
 import Header from "../components/header/header.js";
 import Footer from "../components/footer/footer.js";
 
