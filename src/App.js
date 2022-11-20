99d01360aa740c88b4cf256d38e088800c4e3b70
Jericho FIllon <jerichofilloncoding@gmail.com>
2022-11-19 23:17:20 -0800

    Changes to the package.json for test deployment to Netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
+import { gsap } from "gsap";
 
 import "./App.css";
 
@@ -14,6 +15,8 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [countDown, setCountDown] = useState(0);
 
+  // The useEffect Hook allows you to perform side effects in your components.
+  // Some examples of side effects are: fetching data, directly updating the DOM, and timers.
   useEffect(() => {
     let timerId = null;
 
@@ -30,10 +33,23 @@ const App = () => {
 
     setTimeout(() => {
       setLoading(false);
-      //Stops memory leaks
       clearInterval(timerId);
+
+      console.log("Loading finished.");
     }, 4000);
   }, []);
+
+  // After DOM has been rendered
+  useLayoutEffect(() => {
+    console.log("Run animations.");
+    // Using Learn GSAP in 23m by WDS, you can pass objects inside gsap.to(); - Video: 16:58 / 23:21
+    // For example; 
+    // 
+    // const obj = { x: 0 }
+    // gsap.to(obj, { x : 100, duration: 4, onUpdate() => console.log(obj.x)})
+    // 
+    // console will print from 0 to 100;
+  });
 
   return (
     <div className="App">
