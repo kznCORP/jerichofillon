c860c471959058d52f661509b4fd3ee718be1fb4
Jericho FIllon <jerichofilloncoding@gmail.com>
2022-11-22 00:16:29 -0800

    GSAP animation for loading screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
+import { gsap } from "gsap";
 
 import "./App.css";
 
@@ -14,8 +15,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [countDown, setCountDown] = useState(0);
 
-  // The useEffect Hook allows you to perform side effects in your components.
-  // Some examples of side effects are: fetching data, directly updating the DOM, and timers.
+  // The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers.
   useEffect(() => {
     let timerId = null;
 
@@ -30,25 +30,45 @@ const App = () => {
       }, 100);
     }
 
+    // What our code is doing. Let's clean it up!
+    //
+    // Loading renders
+    // -> useEffect() gets called
+    // -> Timer goes 0-100
+    // -> unmount Loading Screen
+    // -> stop Timer
+    //
+    // Homepage renders
+    // -> portal fade ease-in
     setTimeout(() => {
-      setLoading(false);
       clearInterval(timerId);
-
       console.log("Loading finished.");
     }, 4000);
   }, []);
 
-  // After DOM has been rendered
   useLayoutEffect(() => {
-    console.log("Run animations.");
-    // Using Learn GSAP in 23m by WDS, you can pass objects inside gsap.to(); - Video: 16:58 / 23:21
-    // For example;
-    //
-    // const obj = { x: 0 }
-    // gsap.to(obj, { x : 100, duration: 4, onUpdate() => console.log(obj.x)})
-    //
-    // console will print from 0 to 100;
-  });
+    setTimeout(() => {
+      setLoading(false);
+      console.log("Run useLayoutEffect");
+
+      const timeline = gsap.timeline();
+      timeline
+        .from(".portalWrapper", { opacity: 0, duration: 2, ease: "circ.in" })
+        .from("#header", { opacity: 0, y: 25, duration: 1.5, ease: "slow" })
+        .from("#slogan", { opacity: 0, y: 25, duration: 1.5, ease: "slow" });
+
+      document.querySelectorAll(".portfolioLinkWrapper").forEach((link, i) => {
+        console.log("Run QSA");
+
+        gsap.from(link, {
+          opacity: 0,
+          y: 25,
+          duration: i + 0.5,
+          delay: 5,
+        });
+      });
+    }, 4000);
+  }, []);
 
   return (
     <div className="App">
@@ -67,3 +87,11 @@ const App = () => {
 };
 
 export default App;
+
+// Using Learn GSAP in 23m by WDS, you can pass objects inside gsap.to(); - Video: 16:58 / 23:21
+// For example;
+//
+// const obj = { x: 0 }
+// gsap.to(obj, { x : 100, duration: 4, onUpdate() => console.log(obj.x)})
+//
+// console will print from 0 to 100;
