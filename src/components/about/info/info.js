1d28ba1f68a5bef7a5379d3f0980b821382a4641
Jericho FIllon <jerichofilloncoding@gmail.com>
2022-12-15 15:01:08 -0800

    Added _redirects & netlify.toml to fix Netlifys refresh broken link

--- src/components/about/info/info.js
+++ src/components/about/info/info.js
@@ -11,9 +11,6 @@ const Info = () => {
     <section id="info">
       <div id="aboutMe">
         <article id="infoOccupation">
-          <div>
-            <h6 className="subheading">Jericho Fillon</h6>
-          </div>
           <h1 className="infoOccupation infoDesc">
             A multidisciplinary Designer focusing on helping brands, products
             and people make a mark.
@@ -94,19 +91,19 @@ const Info = () => {
         <div className="contactContainer">
           <article id="contactDesc">
             <div>
-              <h6 className="subheading">Open Availability</h6>
+              <h6 className="subheading openAvailability">Open Availability</h6>
             </div>
             <h2 className="contactMe">Let's talk about your next project.</h2>
           </article>
 
-          <div id='speechBubble'>
+          <div id="speechBubble">
             <a
               href="mailto:fillonjericho@gmail.com?subject=Project%20Inquiry&body=Hello%2C%20thank%20you%20for%20taking%20the%20time%20to%20surf%20through%20my%20portfolio%20and%20reach%20out%20%3A-)%0D%0A%0D%0APlease%20fill%20in%20the%20necessary%20information%20so%20that%20we%20can%20work%20together!%0D%0A%0D%0AName%3A%0D%0ACompany%3A%0D%0AGoal%3A%0D%0ACurrent%20Website(if%20applicable)%3A%0D%0ACompetitors(if%20applicable)%3A%0D%0ADesired%20Platform%20(WordPress%2FShopify%2FWebflow%2FCustom)%3A%20%0D%0ATimeline%20(deadline%2Flaunch%20date)%3A%0D%0ABudget%3A%0D%0A%0D%0A%0D%0AAny%20additional%20comments%20you'd%20like%20to%20mention%3A%0D%0A%0D%0A%0D%0A*Upon%20sending%20this%20email%2C%20I%20will%20reply%20back%20within%20the%20next%2024%20hours%20to%20accommodate%20your%20inquiry*"
               target="_blank"
               rel="noopener noreferrer"
             >
               <div className="contactMailWrapper">
-                <SpeechBubbleSVG  />
+                <SpeechBubbleSVG />
                 <button className="contactMail" id="mail" type="button">
                   <h4>hello@jerichofillon.com</h4>
                 </button>
