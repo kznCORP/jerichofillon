37d5d8f8b82a74f45bf120b7eef32691a5c5f335
Jericho FIllon <jerichofilloncoding@gmail.com>
2022-12-18 16:31:04 -0800

    Fixed mobile viewports on iOS using webkit-fill-available

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -5,55 +5,57 @@ import "./portfolio.css";
 const Portfolio = ({ data }) => {
   return (
     <>
-      <section id="portfolio">
-        <div className="pfWrapper">
-          {data.map((project) => {
-            return (
-              <div className="pfCard" id={project.styles} key={project.id}>
-                <div className="pfTop">
-                  <div>
-                    <p className="subheaders">/ Role</p>
+      <main>
+        <section id="portfolio">
+          <div className="pfWrapper">
+            {data.map((project) => {
+              return (
+                <div className="pfCard" id={project.styles} key={project.id}>
+                  <div className="pfTop">
+                    <div>
+                      <p className="subheaders">/ Role</p>
 
-                    {project.roles.map((role) => {
-                      return (
-                        <h3 className="roles" key={role}>
-                          {role}
-                        </h3>
-                      );
-                    })}
+                      {project.roles.map((role) => {
+                        return (
+                          <h3 className="roles" key={role}>
+                            {role}
+                          </h3>
+                        );
+                      })}
+                    </div>
+
+                    <div className="pfLogoWrapper">
+                      <p className="subheaders">/ Logo</p>
+                      <img src={project.src} className="pfLogo" alt="Logo" />
+                    </div>
                   </div>
 
-                  <div className="pfLogoWrapper">
-                    <p className="subheaders">/ Logo</p>
-                    <img src={project.src} className="pfLogo" alt="Logo" />
+                  <div className="pfBottom">
+                    <div>
+                      <p className="subheaders">/ Project</p>
+                      <a
+                        href={project.url}
+                        role="button"
+                        tabIndex="0"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <h3 className="pfTitle">{project.name}</h3>
+                      </a>
+                    </div>
+
+                    <div>
+                      <a href={project.url}>
+                        <h6 className="pfLink">Visit &#8594;</h6>
+                      </a>
+                    </div>
                   </div>
                 </div>
-
-                <div className="pfBottom">
-                  <div>
-                    <p className="subheaders">/ Project</p>
-                    <a
-                      href={project.url}
-                      role="button"
-                      tabIndex="0"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <h3 className="pfTitle">{project.name}</h3>
-                    </a>
-                  </div>
-
-                  <div>
-                    <a href={project.url}>
-                      <h6 className="pfLink">Visit &#8594;</h6>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </section>
+              );
+            })}
+          </div>
+        </section>
+      </main>
     </>
   );
 };
